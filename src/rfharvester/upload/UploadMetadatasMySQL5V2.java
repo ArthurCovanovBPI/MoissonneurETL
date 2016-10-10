3f92056f4fb49d8d4019aeb140abb3e6c8e13cf6
Yhtill <arthur.covanov@bpi.fr>
2016-10-10 11:19:20 +0200

    Some minor updates

--- src/rfharvester/upload/UploadMetadatasMySQL5V2.java
+++ src/rfharvester/upload/UploadMetadatasMySQL5V2.java
@@ -199,7 +199,7 @@ public class UploadMetadatasMySQL5V2 implements RFHarvesterUploaderV2Interface
 		String dc_title = RFHarvesterUtilities.arrayListToString(row.get("titres"), "; ");
 		String dc_creator = RFHarvesterUtilities.arrayListToString(row.get("auteurs"), "; ");
 		String dc_subject = RFHarvesterUtilities.arrayListToString(row.get("sujets"), "; ");
-		System.out.println("dc_subject: " + dc_subject);
+//		System.out.println("dc_subject: " + dc_subject);
 		String dc_description = RFHarvesterUtilities.arrayListToString(row.get("descriptions"), "; ");
 		String dc_publisher = RFHarvesterUtilities.arrayListToString(row.get("editeurs"), "; ");
 		String dc_contributor = RFHarvesterUtilities.arrayListToString(row.get("contributeurs"), "; ");
@@ -305,7 +305,7 @@ public class UploadMetadatasMySQL5V2 implements RFHarvesterUploaderV2Interface
 				throw new RFHarvesterUploaderV2Exception(e);
 			}
 
-			System.out.println(query);
+//			System.out.println(query);
 
 //			RFHarvesterLogger.debug(query);
 			uploadDBStatement = uploadDBConnection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
